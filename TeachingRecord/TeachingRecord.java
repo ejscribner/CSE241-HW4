907586ac72de3c460e7718cae4a7e722d0b1fc2f
ejscribner <ejscribner@gmail.com>
2019-02-18 11:52:22 -0500

    working substring lookup

--- TeachingRecord/TeachingRecord.java
+++ TeachingRecord/TeachingRecord.java
@@ -5,6 +5,7 @@
  */
 
 import java.io.Console;
+import java.net.IDN;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -18,60 +19,40 @@ public class TeachingRecord {
         //create scanner
         Scanner scan = new Scanner(System.in);
 
-
         readLogin(scan, console, "Welcome to my cse241 hw4 project! Please log in below.");
+
         while(!loggedIn) {
             try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241", username, password);
                  Statement stmt = con.createStatement())
             {
-                System.out.println("Connection Succesful.");
+                System.out.println("Connection Succesful.\n");
                 loggedIn = true;
-                String query = generateSubstringQuery(scan);
-                ResultSet result1;
-                result1 = stmt.executeQuery(query);
-                if(!result1.next()) {
-                    boolean queryIsEmpty = true;
-                    while(queryIsEmpty) {
-                        System.out.println("Empty Result.");
-                        query = generateSubstringQuery(scan);
-                        result1 = stmt.executeQuery(query);
-                    }
+                printResult1(scan, stmt);
+            } catch (SQLException sqe) {
+                System.out.println("code: " + sqe.getErrorCode());
+                System.out.println("msg: " + sqe.getMessage());
+                if(sqe.getErrorCode() == 28000) {
+                    readLogin(scan, console, "Error: Your Account is Locked, Please Try Again");
                 }
-                System.out.println(" ID    " + "|" + "  Name");
-                System.out.println("---------------");
-                do {
-                    System.out.println(result1.getString("ID") + " | " + result1.getString("Name"));
-                } while(result1.next());
+                if(sqe.getErrorCode() == 1017) {
+                    readLogin(scan, console, "Error: Invalid Username or Password");
+                }
+                if(sqe.getMessage().contains("No suitable driver found")) {
+                    readLogin(scan, console, "Error: Could Not Connect, Driver Not Found");
+                }
+                break;
             }
             catch (Exception e) {
-                System.out.println("1");
-                if(e.getMessage() == "No suitable driver found for jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241") {
-                    System.out.println("2");
-                    readLogin(scan, console, "Error: Could Not Connect, Driver Not Found");
-                    loggedIn = false;
-                }
-                System.out.println("3");
-                if(e.getMessage() == "ORA-01017: invalid username/password; logon denied") {
-                    System.out.println("4");
-                    readLogin(scan, console, "Error: Could Not Connect, Invalid Password");
-                    loggedIn = false;
-                }
-                System.out.println("5");
-                if(e.getMessage() == "ORA-00942: table or view does not exist") {
-                    System.out.println("6");
-                    System.out.println("Error: Invalid Query, table or view does not exist.");
-                }
-                System.out.println("7");
-                if(e.getMessage() == "ORA-28000: The account is locked.") {
-                    System.out.println("8");
-                    readLogin(scan, console, "Error: Your Account is Locked");
-                    loggedIn = false;
-                }
-                System.out.println("8");
                 //find more error cases by connecting to different database, database that is down, etc
                 //query not in dtabase
                 System.out.println("Message: " + e.getMessage());
                 System.out.println("Error: Could Not Connect, We Dont Know Why");
+                break; //infinite looping on account locked for some reason, need to set logged in = true on success
+            }
+            int ctr = 0;
+            ctr++;
+            if (ctr > 5) {
+                System.out.println("Too many failed login attempts.");
                 break;
             }
         }
@@ -86,12 +67,37 @@ public class TeachingRecord {
         System.out.println("Connecting...");
     }
 
-    static String generateSubstringQuery(Scanner scan) {
-        System.out.println("Please enter a search substring for name: ");
+    static ResultSet generateSubstringQuery(Scanner scan, Statement stmt) {
+        System.out.print("Please enter a search substring for name: ");
         String searchSub = scan.next();
         String query;
         query = "select to_char(instructor.id, '00000')as \"ID\", NAME from INSTRUCTOR where NAME like \'%" + searchSub + "%\'";
-        return query;
-        //should add stmt.excecute here and handle exception seperately
+        ResultSet result = null;
+        try {
+            result = stmt.executeQuery(query);
+        } catch (Exception executeException) {
+            System.out.println("Err Msg: " + executeException.getMessage());
+        }
+        return result;
+    }
+
+    static void printResult1(Scanner scan, Statement stmt) {
+        ResultSet result1;
+        result1 = generateSubstringQuery(scan, stmt);
+        try {
+            while (!result1.next()) {
+                System.out.println("Error: Empty Result. Try again.");
+                result1 = generateSubstringQuery(scan, stmt);
+            }
+            System.out.println(" ID    " + "|" + "  Name");
+            System.out.println("---------------");
+            do {
+                System.out.println(result1.getString("ID") + " | " + result1.getString("Name"));
+            } while(result1.next());
+        } catch (SQLException sqe) {
+            System.out.println("SQl Error code: " + sqe.getErrorCode());
+        } catch (Exception e) {
+            System.out.println("Regular error msg: " + e.getMessage());
+        }
     }
 } 
